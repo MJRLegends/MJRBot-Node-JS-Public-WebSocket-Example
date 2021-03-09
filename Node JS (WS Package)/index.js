1c3d051268b648791ae266dd25a1ee70afefb943
MJRLegends <MJRLegends@users.noreply.github.com>
2021-03-09 20:40:03 +0000

    Changed nonce to support MJRBot updates

--- Node JS (WS Package)/index.js
+++ Node JS (WS Package)/index.js
@@ -50,7 +50,7 @@ connection.on('message', function incoming(message) {
 function sendListenRequest() {
 	connection.send(JSON.stringify({
 		"type": "LISTEN",
-		"nonce": "snLwNF1kKeYxKOqHiq19%WEC*2UYuGxMrm6*30b9rFzKC0Yw5$S^2yXT!pyCLob8", // Unique Random 64 or higher string
+		"nonce": "snLwNF1kKeYxKOqHiq195WEC52UYuGxMrm6530b9rFzKC0Yw55S52yXT5pyCLob8", // Unique Random 64 or higher string
 		"channel_id": 13371337,  // Your channels personal twitch id Token can be found from https://mjrbot.mjrlegends.com/developers.php
 		"topics": ["channel_points_reward_redeem"], // Topics to listen too
 		"token": "TOKEN" // Your channels personal WS Token from https://mjrbot.mjrlegends.com/developers.php
